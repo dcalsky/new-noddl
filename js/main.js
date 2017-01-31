cf29fdb01153a047c2f6ec74e22f1fb9f6a4340b
dcalsky <dcalsky@163.com>
2017-01-31 20:58:20 +0800

    add meta

--- js/main.js
+++ js/main.js
@@ -4,9 +4,9 @@ var findElementById = function (id) {
 window.onload = function () {
     var left = findElementById('left-part'), right = findElementById('right-part');
     left.addEventListener('click', function () {
-        window.location.href = 'http://github.com/dcalsky';
+        window.open('http://github.com/dcalsky');
     });
     right.addEventListener('click', function () {
-        window.location.href = 'http://blog.noddl.me';
+        window.open('http://blog.noddl.me');
     });
 };
